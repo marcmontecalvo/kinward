fa5602c7b3e47f4da637f9fac715436868cf0813
Marc <marc.montecalvo@gmail.com>
2026-07-11 19:18:28 -0400

    Tighten Alembic typing

--- services/kinward/migrations/env.py
+++ services/kinward/migrations/env.py
@@ -5,6 +5,7 @@ from logging.config import fileConfig
 
 from alembic import context
 from sqlalchemy import pool
+from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from kinward.config import get_settings
@@ -30,7 +31,7 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
-def do_run_migrations(connection: object) -> None:
+def do_run_migrations(connection: Connection) -> None:
     context.configure(connection=connection, target_metadata=target_metadata, compare_type=True)
     with context.begin_transaction():
         context.run_migrations()
